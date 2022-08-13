21b75455a21a949c21693e8c8e90f9c35906cc55
suyeon12 <mysummer321@gmail.com>
2022-08-13 22:16:51 +0900

    Initialize repo

---
+++ index.js
@@ -0,0 +1,46 @@
+var oracledb = require('oracledb');
+v
+// Express 기본 모듈 불러오기
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+// 익스프레스 객체 생성
+var app = express();
+ 
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+
+
+// body-parser
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// 라우터 객체 참조
+var router = express.Router();
+
+// Oracle Auto Commit 설정
+oracledb.autoCommit = true;
+
+
+router.post('/dbinsert', function(request, response){
+
+    oracledb.getConnection({
+        user            : dbConfig.user,
+        password        : dbConfig.password,
+        connectString   : dbConfig.connectString
+    },
+    function(err, connection) {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+
+        let query = 'INSERT INTO NUTRI(TYPE,NAME,DAY,COUNT)' + 'VALUES(:TYPE, :NAME, :DAY, :COUNT)';
+}
+
+// 라우터 객체를 app 객체에 등록
+app.use('/', router);
